c5e93899d81dc91ebf2e1c30ca8d04320ba1aaf1
Raia Kisseljova <84378854+Raissa-Kisseljova@users.noreply.github.com>
2021-07-15 18:36:58 +0100

    Update Registration.jsx

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -21,15 +21,13 @@ const Registration = () => {
     }
 
 
-
     const onSubmit = (e) => {
         e.preventDefault()
         console.log(e)
-        if (formField.password.value === formField.passwordRepeat.value) {
+        if (formField.password === formField.passwordRepeat) {
             console.log("success")
             console.log()
         } else {
-            <Alert>Passwords doesn't match</Alert>
             console.log("not good")
         }
     }
@@ -46,7 +44,7 @@ const Registration = () => {
                             <input required minLength="3" type="text" placeholder="Surname" id="surname" onChange={(e) => handleInput(['surname'], e.target.value)} />
                             <input type="email" placeholder="Email" id="email" onChange={(e) => handleInput(['email'], e.target.value)} />
                             <input type="password" required minLength="8" placeholder="Password" id="password" onChange={(e) => handleInput(['password'], e.target.value)} />
-                            <input type="password" placeholder="Repeat password" id="password-repeat" onChange={(e) => handleInput(['passwordRepeat'], e.target.value)} />
+                            <input type="password" required minLength="8" placeholder="Repeat password" id="password-repeat" onChange={(e) => handleInput(['passwordRepeat'], e.target.value)} />
                             <button type="submit" onClick={(e) => onSubmit}>Submit</button>
                         </form>
                     </div>
